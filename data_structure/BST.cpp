69ea2181d63a4a682cf0d6bd4586df4b21e38f87
Nghia Lam <nghialam12795@gmail.com>
2020-07-13 19:08:36 +0700

    - Add BST check valid Solution

--- data_structure/BST.cpp
+++ data_structure/BST.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-template <typename T> struct Node {
+template <typename T>
+struct Node {
   T val;
   Node *left;
   Node *right;
@@ -16,7 +17,33 @@ template <typename T> struct Node {
   }
 };
 
+template <typename T>
+class Solution {
+public:
+  static bool is_valid_tree(Node<T> *root) {
+    return is_valid_tree(root, std::numeric_limits<T>::min(),
+                         std::numeric_limits<T>::max());
+  }
+
+  static bool is_valid_tree(Node<T> *root, T bound_low, T bound_high) {
+    // Null node is also valid in Binary Search Tree
+    if (!root)
+      return true;
+
+    // Checking if left and right is valid
+    if (root->val > bound_low && root->val < bound_high &&
+        is_valid_tree(root->left, bound_low, root->val) &&
+        is_valid_tree(root->right, root->val, bound_high))
+      return true;
+
+    return false;
+  }
+};
+
 int main() {
-    Node<int> node(2);
-    node.left = new Node<int>(5);
+  Node<int> node(5);
+  node.left = new Node<int>(2);
+  node.right = new Node<int>(7);
+
+  std::cout << Solution<int>().is_valid_tree(&node) << std::endl;
 }
